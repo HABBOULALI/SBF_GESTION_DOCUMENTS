f0fb718a881878c35adfc9b74e0802b199a303e2
HABBOULALI <smq.sfax@sbf.com.tn>
2026-02-24 10:40:50 +0100

    refactor: Simplify React app initialization
    Remove redundant checks and streamline the root element mounting process for the React application. Also, remove the strict mode wrapper as it's generally not needed for the main application entry point.

--- index.tsx
+++ index.tsx
@@ -3,15 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 
 const container = document.getElementById('root');
-const root = createRoot(container!);
-root.render(<App />);
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+if (container) {
+  const root = createRoot(container);
+  root.render(<App />);
 }
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
